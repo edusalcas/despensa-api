b8ff65b3f9515721b291c224ead920b8d5ceebff
Jezuh <jesusmarinaviles98@gmail.com>
2024-03-16 13:45:20 +0100

    Initial version of ‘edit recipe’ feature added; no functionality implemented yet.

--- despensa_angular/src/app/components/recipes/edit-recipe/edit-recipe.component.ts
+++ despensa_angular/src/app/components/recipes/edit-recipe/edit-recipe.component.ts
@@ -1,7 +1,11 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {NgForOf, NgIf} from "@angular/common";
 import {NgSelectModule} from "@ng-select/ng-select";
-import {ReactiveFormsModule} from "@angular/forms";
+import {FormBuilder, FormGroup, ReactiveFormsModule} from "@angular/forms";
+import {Recipe} from "../../../entities/recipe";
+import {RecipesService} from "../../../services/recipes_service/recipes.service";
+import {Subscription} from "rxjs";
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-edit-recipe',
@@ -15,6 +19,40 @@ import {ReactiveFormsModule} from "@angular/forms";
   templateUrl: './edit-recipe.component.html',
   styleUrl: './edit-recipe.component.css'
 })
-export class EditRecipeComponent {
+export class EditRecipeComponent implements OnInit, OnDestroy {
+
+  protected recipe: Recipe | undefined;
+  private subs: Subscription[] = [];
+  form: FormGroup;
+
+  constructor(private recipeServ: RecipesService,
+              private router: ActivatedRoute,
+              private fb: FormBuilder) {
+    this.form = this.fb.group({
+
+    });
+  }
+
+  ngOnInit() {
+    let id = -1;
+    this.subs.push(this.router.params.subscribe({
+      next: value => {
+        id = value["id"];
+      }
+    }));
+
+    this.subs.push(this.recipeServ.get(id).subscribe({
+      next: data => {
+        this.recipe = data;
+      },
+      error: err => {
+        console.error(err);
+      }
+    }));
+  }
+
+  ngOnDestroy(): void {
+    this.subs.forEach(sub => sub.unsubscribe());
+  }
 
 }
